14c11f04159cc65cf88bac31c510257af1acd525
ONP <74316362+ONP4AM@users.noreply.github.com>
2023-10-27 17:35:44 +0530

    Update mp4owner.ps1

--- mp4owner.ps1
+++ mp4owner.ps1
@@ -5,6 +5,5 @@ foreach ($file in $mp4Files) {
     # Take ownership
     takeown /f $file.FullName
 
-    # Grant permissions to a specific user or group (replace S-1-5-11 with the desired SID)
     icacls $file.FullName /grant "*S-1-5-11:(R)"
 }
