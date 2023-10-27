b480e56e28382ca4d37f911b5afe0056ad40b131
ONP <74316362+ONP4AM@users.noreply.github.com>
2023-10-27 17:35:33 +0530

    Update ownership.ps1

--- ownership.ps1
+++ ownership.ps1
@@ -5,6 +5,5 @@ foreach ($folder in $folders) {
     # Take ownership
     takeown /f $folder.FullName
 
-    # Grant permissions to a specific user or group (replace S-1-5-11 with the desired SID)
     icacls $folder.FullName /grant "*S-1-5-11:(R)"
 }
