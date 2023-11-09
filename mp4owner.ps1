19fcad8b2ebcb3d30c7876898f7fea2cddb7b066
ONP <74316362+ONP4AM@users.noreply.github.com>
2023-11-09 13:42:40 +0530

    Update mp4owner.ps1

--- mp4owner.ps1
+++ mp4owner.ps1
@@ -1,9 +1,1 @@
-$mp4Files = Get-ChildItem -Filter *.mp4
 
-# Loop through each .mp4 file and apply the ownership and permissions changes
-foreach ($file in $mp4Files) {
-    # Take ownership
-    takeown /f $file.FullName
-
-    icacls $file.FullName /grant "*S-1-5-11:(R)"
-}
